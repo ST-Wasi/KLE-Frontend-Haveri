a63905bb62442c7a3eff0db96040e27c5261014b
wasiowision <wasi@owision.com>
2025-02-19 11:45:21 +0530

    Update API URL to production endpoint

--- src/api/cart.js
+++ src/api/cart.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:4242";
+const API_URL = "https://kle-com-backend.onrender.com";
 
 export async function addToCart(product) {
   const response = await fetch(`${API_URL}/cart/add`, {
